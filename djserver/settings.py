b484b551d64cbaf990aa57f850df818aafaccc4f
mdsafeeuddin <mdsafeeuddin@gmail.com>
2024-04-21 16:38:50 +0530

    updated static files path.

--- djserver/settings.py
+++ djserver/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/drone_server/admin'
+STATIC_ROOT = '/home/drone_server/admin/admin'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
